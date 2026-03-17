250206a6123ed33165b6b5a0da8afedda7610d81
unknown <henzo_25995@ETEC.FURLAN>
2026-03-16 21:56:34 -0300

    Adicionados

--- components/data/pets.js
+++ components/data/pets.js
@@ -3,7 +3,7 @@ export const pets = [
     id: "1",
     type: "Cat",
     name: "Crepusculo",
-    breed: "Siamese",
+    breed: "persa",
     age: "2 anos",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_ai5gloAQiZqnEsgVv1vtRNH0JG1z_0Y2aw&s"
   },
@@ -19,14 +19,14 @@ export const pets = [
     id: "3",
     type: "Papagaio",
     name: "Kiwi",
-    breed: "Psittacus erithacus",
+    breed: "papagaio do mangue",
     age: "1 ano",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTW950r5Mr1PGMCvljpPd1rPKReU89NLSuWw&s"
   },
    {
     id: "4",
     type: "Cobra",
-    name: " ",
+    name: "raivoso ",
     breed: "Cascavel",
     age: "1 ano",
     image: "https://s2-g1.glbimg.com/NTei1f8X2BuuFJ1Ul4_9FSZpSvM=/1280x0/filters:format(jpeg)/https://i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2022/S/Y/eQVfFhTiGTXwqawQqpAg/whatsapp-image-2022-06-07-at-15.53.06.jpeg"
